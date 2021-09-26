3db073bd0840df6c88b1f5318f2545bbadb3b3ea
Scott Smitelli <scott@smitelli.com>
2021-09-25 21:22:08 -0400

    Fill out DNS records for NFM-com

--- dns-northernflickermusic-com.tf
+++ dns-northernflickermusic-com.tf
@@ -5,6 +5,35 @@ resource "linode_domain" "northernflickermusic-com" {
   tags      = ["northernflickermusic.com"]
 }
 
+resource "linode_domain_record" "a_northernflickermusic-com" {
+  domain_id   = linode_domain.northernflickermusic-com.id
+  record_type = "A"
+  name        = ""
+  target      = local.alala_ips.v4
+}
+
+resource "linode_domain_record" "aaaa_northernflickermusic-com" {
+  domain_id   = linode_domain.northernflickermusic-com.id
+  record_type = "AAAA"
+  name        = ""
+  target      = local.alala_ips.v6
+}
+
+resource "linode_domain_record" "cname_www-northernflickermusic-com" {
+  domain_id   = linode_domain.northernflickermusic-com.id
+  record_type = "CNAME"
+  name        = "www"
+  target      = linode_domain.northernflickermusic-com.domain
+}
+
+resource "linode_domain_record" "caa_northernflickermusic-com" {
+  domain_id   = linode_domain.northernflickermusic-com.id
+  record_type = "CAA"
+  name        = ""
+  tag         = "issue"
+  target      = "letsencrypt.org"
+}
+
 resource "linode_domain_record" "txt_spf_northernflickermusic-com" {
   domain_id   = linode_domain.northernflickermusic-com.id
   record_type = "TXT"
