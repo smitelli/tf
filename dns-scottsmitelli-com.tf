34b73daf176d90c83818e7d5232c55a4476dcd34
Scott Smitelli <scott@smitelli.com>
2022-04-09 19:36:56 -0400

    Try to modularize the Zoho config

--- dns-scottsmitelli-com.tf
+++ dns-scottsmitelli-com.tf
@@ -5,28 +5,11 @@ resource "linode_domain" "scottsmitelli-com" {
   tags      = ["scottsmitelli.com"]
 }
 
-resource "linode_domain_record" "mx_1_scottsmitelli-com" {
-  domain_id   = linode_domain.scottsmitelli-com.id
-  record_type = "MX"
-  name        = ""
-  priority    = 10
-  target      = "mx.zoho.com"
-}
-
-resource "linode_domain_record" "mx_2_scottsmitelli-com" {
-  domain_id   = linode_domain.scottsmitelli-com.id
-  record_type = "MX"
-  name        = ""
-  priority    = 20
-  target      = "mx2.zoho.com"
-}
-
-resource "linode_domain_record" "mx_3_scottsmitelli-com" {
-  domain_id   = linode_domain.scottsmitelli-com.id
-  record_type = "MX"
-  name        = ""
-  priority    = 50
-  target      = "mx3.zoho.com"
+module "zohomail_scottsmitelli-com" {
+  source            = "./modules/zohomail"
+  domain_id         = linode_domain.scottsmitelli-com.id
+  dkim_public_key   = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCYrIZRkcmANSjqczdsVwsAJjSj6ip4MbrO7kwaCy7v/f6hRzSKbInq3lGXl6xYmY7mN2Xfa9ZkWupjMVh6Dyg/xQT/wj5PQti0eW91XQ4nylclt2k4iSH0jSkjgCT6ZYUvWo25EbNmEVscs2wFWlG0FFbQLVAhIEcL8IE1qty69wIDAQAB"
+  verification_code = "zb94673074.zmverify.zoho.com"
 }
 
 resource "linode_domain_record" "a_scottsmitelli-com" {
@@ -72,20 +55,6 @@ resource "linode_domain_record" "caa_scottsmitelli-com" {
   target      = "letsencrypt.org"
 }
 
-resource "linode_domain_record" "txt_spf_scottsmitelli-com" {
-  domain_id   = linode_domain.scottsmitelli-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "v=spf1 include:zoho.com ~all"
-}
-
-resource "linode_domain_record" "txt_dkim_scottsmitelli-com" {
-  domain_id   = linode_domain.scottsmitelli-com.id
-  record_type = "TXT"
-  name        = "zmail._domainkey"
-  target      = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCYrIZRkcmANSjqczdsVwsAJjSj6ip4MbrO7kwaCy7v/f6hRzSKbInq3lGXl6xYmY7mN2Xfa9ZkWupjMVh6Dyg/xQT/wj5PQti0eW91XQ4nylclt2k4iSH0jSkjgCT6ZYUvWo25EbNmEVscs2wFWlG0FFbQLVAhIEcL8IE1qty69wIDAQAB"
-}
-
 resource "linode_domain_record" "txt_gsv_scottsmitelli-com" {
   domain_id   = linode_domain.scottsmitelli-com.id
   record_type = "TXT"
@@ -98,11 +67,4 @@ resource "linode_domain_record" "txt_ksv_scottsmitelli-com" {
   record_type = "TXT"
   name        = ""
   target      = "keybase-site-verification=vZzkuUFcKVMRBbJn8Q2w4qNJeYd5c9hqWi1YollNV44"
-}
-
-resource "linode_domain_record" "txt_zv_scottsmitelli-com" {
-  domain_id   = linode_domain.scottsmitelli-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "zoho-verification=zb94673074.zmverify.zoho.com"
 }
---
+++ modules/zohomail/main.tf
@@ -0,0 +1,65 @@
+variable "domain_id" {
+  description = "The identifier of the Linode domain to attach the records to"
+  type        = int
+}
+
+variable "dkim_public_key" {
+  description = "The public key material for the DKIM p=... element"
+  type        = string
+  default     = null
+}
+
+variable "verification_code" {
+  description = "The value to insert in the zoho-verification=... TXT record"
+  type        = string
+  default     = null
+}
+
+resource "linode_domain_record" "mx_1" {
+  domain_id   = var.domain_id
+  record_type = "MX"
+  name        = ""
+  priority    = 10
+  target      = "mx.zoho.com"
+}
+
+resource "linode_domain_record" "mx_2" {
+  domain_id   = var.domain_id
+  record_type = "MX"
+  name        = ""
+  priority    = 20
+  target      = "mx2.zoho.com"
+}
+
+resource "linode_domain_record" "mx_3" {
+  domain_id   = var.domain_id
+  record_type = "MX"
+  name        = ""
+  priority    = 50
+  target      = "mx3.zoho.com"
+}
+
+resource "linode_domain_record" "txt_spf" {
+  domain_id   = var.domain_id
+  record_type = "TXT"
+  name        = ""
+  target      = "v=spf1 include:zoho.com ~all"
+}
+
+resource "linode_domain_record" "txt_dkim" {
+  count = (var.dkim_public_key == null) ? 0 : 1
+
+  domain_id   = var.domain_id
+  record_type = "TXT"
+  name        = "zmail._domainkey"
+  target      = "v=DKIM1; k=rsa; p=${var.dkim_public_key}"
+}
+
+resource "linode_domain_record" "txt_verification" {
+  count = (var.verification_code == null) ? 0 : 1
+
+  domain_id   = var.domain_id
+  record_type = "TXT"
+  name        = ""
+  target      = "zoho-verification=${var.verification_code}"
+}
