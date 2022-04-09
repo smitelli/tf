f15ec35fad7e0b5063c49bc4a830499fe86d4ae8
Scott Smitelli <scott@smitelli.com>
2022-04-09 19:52:53 -0400

    Okay, fine, I'll structure it nicely

--- modules/zohomail/main.tf
+++ modules/zohomail/main.tf
@@ -1,25 +1,9 @@
 terraform {
   required_providers {
     linode = {
+      source  = "linode/linode"
     }
   }
-}
-
-variable "domain_id" {
-  description = "The identifier of the Linode domain to attach the records to"
-  type        = number
-}
-
-variable "dkim_public_key" {
-  description = "The public key material for the DKIM p=... element"
-  type        = string
-  default     = null
-}
-
-variable "verification_code" {
-  description = "The value to insert in the zoho-verification=... TXT record"
-  type        = string
-  default     = null
 }
 
 resource "linode_domain_record" "mx_1" {
