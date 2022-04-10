a14c4d775b9d4f1998d4bec46314f79349141c97
Scott Smitelli <scott@smitelli.com>
2022-04-10 10:12:09 -0400

    Modularize the big kahuna and remove XMPP cruft

--- dns-smitelli-com.tf
+++ dns-smitelli-com.tf
@@ -5,6 +5,16 @@ resource "linode_domain" "smitelli-com" {
   tags      = ["smitelli.com"]
 }
 
+module "base_smitelli-com" {
+  source = "./modules/base_domain"
+
+  domain_id                = linode_domain.smitelli-com.id
+  domain_name              = linode_domain.smitelli-com.domain
+  primary_ipv4             = [local.alala_ips.v4]
+  primary_ipv6             = [local.alala_ips.v6]
+  google_verification_code = "RZjE9cMMrIEZWmZ5UOWQSohaHPpT6_kQkNkeSkyZ3c0"
+}
+
 resource "linode_domain_record" "mx_1_smitelli-com" {
   domain_id   = linode_domain.smitelli-com.id
   record_type = "MX"
@@ -45,68 +55,6 @@ resource "linode_domain_record" "mx_5_smitelli-com" {
   target      = "alt4.aspmx.l.google.com"
 }
 
-resource "linode_domain_record" "srv_1_xmpp-server-tcp-smitelli-com" {
-  domain_id   = linode_domain.smitelli-com.id
-  record_type = "SRV"
-  service     = "xmpp-server"
-  protocol    = "tcp"
-  priority    = 5
-  weight      = 0
-  port        = 5269
-  target      = "xmpp-server.l.google.com"
-}
-
-resource "linode_domain_record" "srv_2_xmpp-server-tcp-smitelli-com" {
-  domain_id   = linode_domain.smitelli-com.id
-  record_type = "SRV"
-  service     = "xmpp-server"
-  protocol    = "tcp"
-  priority    = 20
-  weight      = 0
-  port        = 5269
-  target      = "alt1.xmpp-server.l.google.com"
-}
-
-resource "linode_domain_record" "srv_3_xmpp-server-tcp-smitelli-com" {
-  domain_id   = linode_domain.smitelli-com.id
-  record_type = "SRV"
-  service     = "xmpp-server"
-  protocol    = "tcp"
-  priority    = 20
-  weight      = 0
-  port        = 5269
-  target      = "alt2.xmpp-server.l.google.com"
-}
-
-resource "linode_domain_record" "srv_4_xmpp-server-tcp-smitelli-com" {
-  domain_id   = linode_domain.smitelli-com.id
-  record_type = "SRV"
-  service     = "xmpp-server"
-  protocol    = "tcp"
-  priority    = 20
-  weight      = 0
-  port        = 5269
-  target      = "alt3.xmpp-server.l.google.com"
-}
-
-resource "linode_domain_record" "srv_5_xmpp-server-tcp-smitelli-com" {
-  domain_id   = linode_domain.smitelli-com.id
-  record_type = "SRV"
-  service     = "xmpp-server"
-  protocol    = "tcp"
-  priority    = 20
-  weight      = 0
-  port        = 5269
-  target      = "alt4.xmpp-server.l.google.com"
-}
-
-resource "linode_domain_record" "a_smitelli-com" {
-  domain_id   = linode_domain.smitelli-com.id
-  record_type = "A"
-  name        = ""
-  target      = local.alala_ips.v4
-}
-
 resource "linode_domain_record" "a_acy1-smitelli-com" {
   domain_id   = linode_domain.smitelli-com.id
   record_type = "A"
@@ -129,13 +77,6 @@ resource "linode_domain_record" "a_rdu1-smitelli-com" {
   lifecycle {
     ignore_changes = [target]
   }
-}
-
-resource "linode_domain_record" "aaaa_smitelli-com" {
-  domain_id   = linode_domain.smitelli-com.id
-  record_type = "AAAA"
-  name        = ""
-  target      = local.alala_ips.v6
 }
 
 resource "linode_domain_record" "aaaa_acy1v6-smitelli-com" {
@@ -225,33 +166,11 @@ resource "linode_domain_record" "cname_webmail-smitelli-com" {
   target      = "ghs.googlehosted.com"
 }
 
-resource "linode_domain_record" "cname_www-smitelli-com" {
-  domain_id   = linode_domain.smitelli-com.id
-  record_type = "CNAME"
-  name        = "www"
-  target      = linode_domain.smitelli-com.domain
-}
-
-resource "linode_domain_record" "caa_smitelli-com" {
-  domain_id   = linode_domain.smitelli-com.id
-  record_type = "CAA"
-  name        = ""
-  tag         = "issue"
-  target      = "letsencrypt.org"
-}
-
 resource "linode_domain_record" "txt_spf_smitelli-com" {
   domain_id   = linode_domain.smitelli-com.id
   record_type = "TXT"
   name        = ""
   target      = "v=spf1 include:_spf.google.com a ~all"
-}
-
-resource "linode_domain_record" "txt_gsv_smitelli-com" {
-  domain_id   = linode_domain.smitelli-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "google-site-verification=RZjE9cMMrIEZWmZ5UOWQSohaHPpT6_kQkNkeSkyZ3c0"
 }
 
 resource "linode_domain_record" "txt_ksv_smitelli-com" {
