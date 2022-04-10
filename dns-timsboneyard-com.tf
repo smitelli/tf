189428bea6a7fb51ee34ec025cb28e3161fcacef
Scott Smitelli <scott@smitelli.com>
2022-04-10 09:23:44 -0400

    Modularize what I can in TB

--- dns-timsboneyard-com.tf
+++ dns-timsboneyard-com.tf
@@ -5,6 +5,14 @@ resource "linode_domain" "timsboneyard-com" {
   tags      = ["timsboneyard.com"]
 }
 
+module "base_timsboneyard-com" {
+  source = "./modules/base_domain"
+
+  domain_id                = linode_domain.timsboneyard-com.id
+  domain_name              = linode_domain.timsboneyard-com.domain
+  google_verification_code = "K2HqsxpiQNjAfRPT7SVoDiAiAIbQM8Fa0VluVG5NHDU"
+}
+
 module "zohomail_timsboneyard-com" {
   source = "./modules/zohomail"
 
@@ -17,7 +25,7 @@ resource "linode_domain_record" "a_timsboneyard-com" {
   domain_id   = linode_domain.timsboneyard-com.id
   record_type = "A"
   name        = ""
-  target      = local.dummy_ip_v4 # Maintained by dyndns service on acy1
+  target      = local.dummy_ip_v4 # Maintained by dyndns on calcabrina.acy1
   ttl_sec     = 300
 
   lifecycle {
@@ -37,26 +45,4 @@ resource "linode_domain_record" "aaaa_listen-timsboneyard-com" {
   record_type = "AAAA"
   name        = "listen"
   target      = local.alala_ips.v6
-}
-
-resource "linode_domain_record" "cname_www-timsboneyard-com" {
-  domain_id   = linode_domain.timsboneyard-com.id
-  record_type = "CNAME"
-  name        = "www"
-  target      = linode_domain.timsboneyard-com.domain
-}
-
-resource "linode_domain_record" "caa_timsboneyard-com" {
-  domain_id   = linode_domain.timsboneyard-com.id
-  record_type = "CAA"
-  name        = ""
-  tag         = "issue"
-  target      = "letsencrypt.org"
-}
-
-resource "linode_domain_record" "txt_gsv_timsboneyard-com" {
-  domain_id   = linode_domain.timsboneyard-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "google-site-verification=K2HqsxpiQNjAfRPT7SVoDiAiAIbQM8Fa0VluVG5NHDU"
 }
