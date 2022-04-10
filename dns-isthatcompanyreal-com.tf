8d48b11c5b5bfe1495e66c6b987e5a1cd7b54ac7
Scott Smitelli <scott@smitelli.com>
2022-04-10 11:23:15 -0400

    Mod and Zoho ITCR

--- dns-isthatcompanyreal-com.tf
+++ dns-isthatcompanyreal-com.tf
@@ -1,49 +1,24 @@
 resource "linode_domain" "isthatcompanyreal-com" {
   domain    = "isthatcompanyreal.com"
   type      = "master"
-  soa_email = local.default_soa_email
+  soa_email = "scott+soa@isthatcompanyreal.com"
   tags      = ["isthatcompanyreal.com"]
 }
 
-resource "linode_domain_record" "a_isthatcompanyreal-com" {
-  domain_id   = linode_domain.isthatcompanyreal-com.id
-  record_type = "A"
-  name        = ""
-  target      = local.alala_ips.v4
+module "base_isthatcompanyreal-com" {
+  source = "./modules/base_domain"
+
+  domain_id                = linode_domain.isthatcompanyreal-com.id
+  domain_name              = linode_domain.isthatcompanyreal-com.domain
+  primary_ipv4             = [local.alala_ips.v4]
+  primary_ipv6             = [local.alala_ips.v6]
+  google_verification_code = "eJs1-epD5uHpisYOSt91890geIewuMp1AwSY9K34IqI"
 }
 
-resource "linode_domain_record" "aaaa_isthatcompanyreal-com" {
-  domain_id   = linode_domain.isthatcompanyreal-com.id
-  record_type = "AAAA"
-  name        = ""
-  target      = local.alala_ips.v6
-}
+module "zohomail_isthatcompanyreal-com" {
+  source = "./modules/zohomail"
 
-resource "linode_domain_record" "cname_www-isthatcompanyreal-com" {
-  domain_id   = linode_domain.isthatcompanyreal-com.id
-  record_type = "CNAME"
-  name        = "www"
-  target      = linode_domain.isthatcompanyreal-com.domain
-}
-
-resource "linode_domain_record" "caa_isthatcompanyreal-com" {
-  domain_id   = linode_domain.isthatcompanyreal-com.id
-  record_type = "CAA"
-  name        = ""
-  tag         = "issue"
-  target      = "letsencrypt.org"
-}
-
-resource "linode_domain_record" "txt_spf_isthatcompanyreal-com" {
-  domain_id   = linode_domain.isthatcompanyreal-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "v=spf1 -all"
-}
-
-resource "linode_domain_record" "txt_gsv_isthatcompanyreal-com" {
-  domain_id   = linode_domain.isthatcompanyreal-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "google-site-verification=eJs1-epD5uHpisYOSt91890geIewuMp1AwSY9K34IqI"
+  domain_id         = linode_domain.isthatcompanyreal-com.id
+  #dkim_public_key   = ""
+  verification_code = "zb32704310.zmverify.zoho.com"
 }
