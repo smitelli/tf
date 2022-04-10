cb57b1c1f421d49a80234d6bbf2154bc8a3dc860
Scott Smitelli <scott@smitelli.com>
2022-04-10 10:31:00 -0400

    Drop in DKIM config

--- dns-rmucae8-com.tf
+++ dns-rmucae8-com.tf
@@ -1,7 +1,7 @@
 resource "linode_domain" "rmucae8-com" {
   domain    = "609-1-800-rusty-mcintyres-used-car-automotive-emporium-8.com"
   type      = "master"
-  soa_email = local.default_soa_email
+  soa_email = "scott+soa@609-1-800-rusty-mcintyres-used-car-automotive-emporium-8.com"
   tags      = ["rmucae8.com"]
 }
 
@@ -16,6 +16,6 @@ module "zohomail_rmucae8-com" {
   source = "./modules/zohomail"
 
   domain_id         = linode_domain.rmucae8-com.id
-  #dkim_public_key   = ""
+  dkim_public_key   = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDUktnQSwS1pDZf3N/pU/WDWk+Nw+JUtaHckcgcRddg/qd2Mi5EvpdgHuhX5rk5PpNxgNraKVYdn1D43OMJFe4igNrVTrPS4lqvTJKuVpadEPCIh5XF3OZ6vEkP7HX4sJAV7MUDr43FbjLY4EdnRz2oyuGy0luYz+cNECq283rfrQIDAQAB"
   verification_code = "zb10007808.zmverify.zoho.com"
 }
