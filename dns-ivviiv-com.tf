d4ccc850a01495beef6872039b3bf16c65b8814e
Scott Smitelli <scott@smitelli.com>
2022-04-10 11:14:07 -0400

    Mod and Zohoify ivviiv

--- dns-ivviiv-com.tf
+++ dns-ivviiv-com.tf
@@ -1,49 +1,24 @@
 resource "linode_domain" "ivviiv-com" {
   domain    = "ivviiv.com"
   type      = "master"
-  soa_email = local.default_soa_email
+  soa_email = "scott+soa@ivviiv.com"
   tags      = ["ivviiv.com"]
 }
 
-resource "linode_domain_record" "a_ivviiv-com" {
-  domain_id   = linode_domain.ivviiv-com.id
-  record_type = "A"
-  name        = ""
-  target      = local.alala_ips.v4
+module "base_ivviiv-com" {
+  source = "./modules/base_domain"
+
+  domain_id                = linode_domain.ivviiv-com.id
+  domain_name              = linode_domain.ivviiv-com.domain
+  primary_ipv4             = [local.alala_ips.v4]
+  primary_ipv6             = [local.alala_ips.v6]
+  google_verification_code = "idLyhN42pY9wR4vGGR9CDXoUIMY2CIq6lj92LyGWyRQ"
 }
 
-resource "linode_domain_record" "aaaa_ivviiv-com" {
-  domain_id   = linode_domain.ivviiv-com.id
-  record_type = "AAAA"
-  name        = ""
-  target      = local.alala_ips.v6
-}
+module "zohomail_ivviiv-com" {
+  source = "./modules/zohomail"
 
-resource "linode_domain_record" "cname_www-ivviiv-com" {
-  domain_id   = linode_domain.ivviiv-com.id
-  record_type = "CNAME"
-  name        = "www"
-  target      = linode_domain.ivviiv-com.domain
-}
-
-resource "linode_domain_record" "caa_ivviiv-com" {
-  domain_id   = linode_domain.ivviiv-com.id
-  record_type = "CAA"
-  name        = ""
-  tag         = "issue"
-  target      = "letsencrypt.org"
-}
-
-resource "linode_domain_record" "txt_spf_ivviiv-com" {
-  domain_id   = linode_domain.ivviiv-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "v=spf1 -all"
-}
-
-resource "linode_domain_record" "txt_gsv_ivviiv-com" {
-  domain_id   = linode_domain.ivviiv-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "google-site-verification=idLyhN42pY9wR4vGGR9CDXoUIMY2CIq6lj92LyGWyRQ"
+  domain_id         = linode_domain.ivviiv-com.id
+  #dkim_public_key   = ""
+  verification_code = "zb97081596.zmverify.zoho.com"
 }
