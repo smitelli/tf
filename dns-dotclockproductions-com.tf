be802f0ba5a441ff1933ce8ebb83876a43a87cbc
Scott Smitelli <scott@smitelli.com>
2022-04-10 11:48:31 -0400

    Mod and Zoho DCP

--- dns-dotclockproductions-com.tf
+++ dns-dotclockproductions-com.tf
@@ -1,49 +1,24 @@
 resource "linode_domain" "dotclockproductions-com" {
   domain    = "dotclockproductions.com"
   type      = "master"
-  soa_email = local.default_soa_email
+  soa_email = "scott+soa@dotclockproductions.com"
   tags      = ["dotclockproductions.com"]
 }
 
-resource "linode_domain_record" "a_dotclockproductions-com" {
-  domain_id   = linode_domain.dotclockproductions-com.id
-  record_type = "A"
-  name        = ""
-  target      = local.alala_ips.v4
+module "base_dotclockproductions-com" {
+  source = "./modules/base_domain"
+
+  domain_id                = linode_domain.dotclockproductions-com.id
+  domain_name              = linode_domain.dotclockproductions-com.domain
+  primary_ipv4             = [local.alala_ips.v4]
+  primary_ipv6             = [local.alala_ips.v6]
+  google_verification_code = "Vo6OurSAL43C0TpHsXEieKMLiiDqlS05ZuTFiB354AQ"
 }
 
-resource "linode_domain_record" "aaaa_dotclockproductions-com" {
-  domain_id   = linode_domain.dotclockproductions-com.id
-  record_type = "AAAA"
-  name        = ""
-  target      = local.alala_ips.v6
-}
+module "zohomail_dotclockproductions-com" {
+  source = "./modules/zohomail"
 
-resource "linode_domain_record" "cname_www-dotclockproductions-com" {
-  domain_id   = linode_domain.dotclockproductions-com.id
-  record_type = "CNAME"
-  name        = "www"
-  target      = linode_domain.dotclockproductions-com.domain
-}
-
-resource "linode_domain_record" "caa_dotclockproductions-com" {
-  domain_id   = linode_domain.dotclockproductions-com.id
-  record_type = "CAA"
-  name        = ""
-  tag         = "issue"
-  target      = "letsencrypt.org"
-}
-
-resource "linode_domain_record" "txt_spf_dotclockproductions-com" {
-  domain_id   = linode_domain.dotclockproductions-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "v=spf1 -all"
-}
-
-resource "linode_domain_record" "txt_gsv_dotclockproductions-com" {
-  domain_id   = linode_domain.dotclockproductions-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "google-site-verification=Vo6OurSAL43C0TpHsXEieKMLiiDqlS05ZuTFiB354AQ"
+  domain_id         = linode_domain.dotclockproductions-com.id
+  #dkim_public_key   = ""
+  verification_code = "zb17489101.zmverify.zoho.com"
 }
