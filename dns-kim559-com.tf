6c017182585b5bbca22d9fc96779486031e9f4a9
Scott Smitelli <scott@smitelli.com>
2022-04-10 11:00:58 -0400

    Install DKIM key

--- dns-kim559-com.tf
+++ dns-kim559-com.tf
@@ -18,6 +18,6 @@ module "zohomail_kim559-com" {
   source = "./modules/zohomail"
 
   domain_id         = linode_domain.kim559-com.id
-  #dkim_public_key   = ""
+  dkim_public_key   = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCQ3NjUUtKo9XJ+tiUpW6KrSMK1qHSglgf6AKM+rGVBkJDAWsmGQnZujhbIw1XjEbQxuOHwcS3qfR16/aULsFK52fp7bkLQUZwwbQMLJzi8Yckm91k0zLn+d43ABhoNbmjOxBkLG4R9S/6IPdwLPv3fUpl3B69qACkcWx/U02X91wIDAQAB"
   verification_code = "zb78802349.zmverify.zoho.com"
 }
