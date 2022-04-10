1dc9fe7979938182b776f58b07c6da04b4268cf8
Scott Smitelli <scott@smitelli.com>
2022-04-10 11:31:56 -0400

    Mod and Zoho IS

--- dns-internetstapler-com.tf
+++ dns-internetstapler-com.tf
@@ -1,49 +1,24 @@
 resource "linode_domain" "internetstapler-com" {
   domain    = "internetstapler.com"
   type      = "master"
-  soa_email = local.default_soa_email
+  soa_email = "scott+soa@internetstapler.com"
   tags      = ["internetstapler.com"]
 }
 
-resource "linode_domain_record" "a_internetstapler-com" {
-  domain_id   = linode_domain.internetstapler-com.id
-  record_type = "A"
-  name        = ""
-  target      = local.alala_ips.v4
+module "base_internetstapler-com" {
+  source = "./modules/base_domain"
+
+  domain_id                = linode_domain.internetstapler-com.id
+  domain_name              = linode_domain.internetstapler-com.domain
+  primary_ipv4             = [local.alala_ips.v4]
+  primary_ipv6             = [local.alala_ips.v6]
+  google_verification_code = "TkSqUeFpUlc16-aFqPzfVUS6qP2RWtb7yRDJNOqjrPY"
 }
 
-resource "linode_domain_record" "aaaa_internetstapler-com" {
-  domain_id   = linode_domain.internetstapler-com.id
-  record_type = "AAAA"
-  name        = ""
-  target      = local.alala_ips.v6
-}
+module "zohomail_internetstapler-com" {
+  source = "./modules/zohomail"
 
-resource "linode_domain_record" "cname_www-internetstapler-com" {
-  domain_id   = linode_domain.internetstapler-com.id
-  record_type = "CNAME"
-  name        = "www"
-  target      = linode_domain.internetstapler-com.domain
-}
-
-resource "linode_domain_record" "caa_internetstapler-com" {
-  domain_id   = linode_domain.internetstapler-com.id
-  record_type = "CAA"
-  name        = ""
-  tag         = "issue"
-  target      = "letsencrypt.org"
-}
-
-resource "linode_domain_record" "txt_spf_internetstapler-com" {
-  domain_id   = linode_domain.internetstapler-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "v=spf1 -all"
-}
-
-resource "linode_domain_record" "txt_gsv_internetstapler-com" {
-  domain_id   = linode_domain.internetstapler-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "google-site-verification=TkSqUeFpUlc16-aFqPzfVUS6qP2RWtb7yRDJNOqjrPY"
+  domain_id         = linode_domain.internetstapler-com.id
+  #dkim_public_key   = ""
+  verification_code = "zb39657632.zmverify.zoho.com"
 }
