ead746922ea4517fc9e02780525f7226d9346f7f
Scott Smitelli <scott@smitelli.com>
2022-04-09 21:15:35 -0400

    Try to modularize the Zoho config (#2)
    * Try to modularize the Zoho config
    
    * Bad type
    
    * What is this crap?
    
    * Can I get away with excluding the version?
    
    * One more check
    
    * Okay, fine, I'll structure it nicely
    
    * !!!

---
+++ modules/zohomail/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+    }
+  }
+}
+
+resource "linode_domain_record" "mx_1" {
+  domain_id   = var.domain_id
+  record_type = "MX"
+  name        = ""
+  priority    = 10
+  target      = "mx.zoho.com"
+}
+
+resource "linode_domain_record" "mx_2" {
+  domain_id   = var.domain_id
+  record_type = "MX"
+  name        = ""
+  priority    = 20
+  target      = "mx2.zoho.com"
+}
+
+resource "linode_domain_record" "mx_3" {
+  domain_id   = var.domain_id
+  record_type = "MX"
+  name        = ""
+  priority    = 50
+  target      = "mx3.zoho.com"
+}
+
+resource "linode_domain_record" "txt_spf" {
+  domain_id   = var.domain_id
+  record_type = "TXT"
+  name        = ""
+  target      = "v=spf1 include:zoho.com ~all"
+}
+
+resource "linode_domain_record" "txt_dkim" {
+  count = (var.dkim_public_key == null) ? 0 : 1
+
+  domain_id   = var.domain_id
+  record_type = "TXT"
+  name        = "zmail._domainkey"
+  target      = "v=DKIM1; k=rsa; p=${var.dkim_public_key}"
+}
+
+resource "linode_domain_record" "txt_verification" {
+  count = (var.verification_code == null) ? 0 : 1
+
+  domain_id   = var.domain_id
+  record_type = "TXT"
+  name        = ""
+  target      = "zoho-verification=${var.verification_code}"
+}
