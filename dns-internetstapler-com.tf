30d547f320d31b6192e3661f8d230bc6e37f9ff8
Scott Smitelli <scott@smitelli.com>
2022-04-10 11:36:19 -0400

    Install DKIM

--- dns-internetstapler-com.tf
+++ dns-internetstapler-com.tf
@@ -19,6 +19,6 @@ module "zohomail_internetstapler-com" {
   source = "./modules/zohomail"
 
   domain_id         = linode_domain.internetstapler-com.id
-  #dkim_public_key   = ""
+  dkim_public_key   = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCDqY8ucgcIaxb0gbd+XSqgL91O7yXLX/6KhnicYtxtAZFr6fm+kEUXX/ZgKwrcq+05IWzoCnswWnV0Z3l3saYVInaevlGYgk2VJluIf6Zs5offMBBwsM5RwT5Rd4I4xoGV7SglMp1bcJkafC50AH2LXXrb9gdi+Qkn/gNWhVSe5wIDAQAB"
   verification_code = "zb39657632.zmverify.zoho.com"
 }
