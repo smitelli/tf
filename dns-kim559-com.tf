acbc2f4968e60a907bd72117acefffa1fd8542cb
Scott Smitelli <scott@smitelli.com>
2022-04-10 10:57:24 -0400

    Mod and verify kim559

--- dns-kim559-com.tf
+++ dns-kim559-com.tf
@@ -1,42 +1,23 @@
 resource "linode_domain" "kim559-com" {
   domain    = "kim559.com"
   type      = "master"
-  soa_email = local.default_soa_email
+  soa_email = "scott+soa@kim559.com"
   tags      = ["kim559.com"]
 }
 
-resource "linode_domain_record" "a_kim559-com" {
-  domain_id   = linode_domain.kim559-com.id
-  record_type = "A"
-  name        = ""
-  target      = local.alala_ips.v4
+module "base_kim559-com" {
+  source = "./modules/base_domain"
+
+  domain_id    = linode_domain.kim559-com.id
+  domain_name  = linode_domain.kim559-com.domain
+  primary_ipv4 = [local.alala_ips.v4]
+  primary_ipv6 = [local.alala_ips.v6]
 }
 
-resource "linode_domain_record" "aaaa_kim559-com" {
-  domain_id   = linode_domain.kim559-com.id
-  record_type = "AAAA"
-  name        = ""
-  target      = local.alala_ips.v6
-}
+module "zohomail_kim559-com" {
+  source = "./modules/zohomail"
 
-resource "linode_domain_record" "cname_www-kim559-com" {
-  domain_id   = linode_domain.kim559-com.id
-  record_type = "CNAME"
-  name        = "www"
-  target      = linode_domain.kim559-com.domain
-}
-
-resource "linode_domain_record" "caa_kim559-com" {
-  domain_id   = linode_domain.kim559-com.id
-  record_type = "CAA"
-  name        = ""
-  tag         = "issue"
-  target      = "letsencrypt.org"
-}
-
-resource "linode_domain_record" "txt_spf_kim559-com" {
-  domain_id   = linode_domain.kim559-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "v=spf1 -all"
+  domain_id         = linode_domain.kim559-com.id
+  #dkim_public_key   = ""
+  verification_code = "zb78802349.zmverify.zoho.com"
 }
