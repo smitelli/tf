291eb821c6cad5415204a9fb788a3de238407bd0
Scott Smitelli <scott@smitelli.com>
2022-04-10 20:32:40 -0400

    Try this again

--- dns-onefinalsound-com.tf
+++ dns-onefinalsound-com.tf
@@ -1,23 +1,21 @@
-# TODO Some stupid asswipe is being a goddamn shithead.
-#
-# resource "linode_domain" "onefinalsound-com" {
-#   domain    = "onefinalsound.com"
-#   type      = "master"
-#   soa_email = "scott+soa@onefinalsound.com"
-#   tags      = ["onefinalsound.com"]
-# }
-#
-# module "base_onefinalsound-com" {
-#   source = "./modules/base_domain"
-#
-#   domain_id   = linode_domain.onefinalsound-com.id
-#   domain_name = linode_domain.onefinalsound-com.domain
-# }
-#
-# module "zohomail_onefinalsound-com" {
-#   source = "./modules/zohomail"
-#
-#   domain_id         = linode_domain.onefinalsound-com.id
-#   #dkim_public_key   = ""
-#   verification_code = "zb02514618.zmverify.zoho.com"
-# }
+resource "linode_domain" "onefinalsound-com" {
+  domain    = "onefinalsound.com"
+  type      = "master"
+  soa_email = "scott+soa@onefinalsound.com"
+  tags      = ["onefinalsound.com"]
+}
+
+module "base_onefinalsound-com" {
+  source = "./modules/base_domain"
+
+  domain_id   = linode_domain.onefinalsound-com.id
+  domain_name = linode_domain.onefinalsound-com.domain
+}
+
+module "zohomail_onefinalsound-com" {
+  source = "./modules/zohomail"
+
+  domain_id = linode_domain.onefinalsound-com.id
+  #dkim_public_key   = ""
+  verification_code = "zb02514618.zmverify.zoho.com"
+}
