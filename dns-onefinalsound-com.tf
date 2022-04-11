a3576264c31a26d1e2157eaa4171977628175da6
Scott Smitelli <scott@smitelli.com>
2022-04-10 20:39:57 -0400

    Add DKIM

--- dns-onefinalsound-com.tf
+++ dns-onefinalsound-com.tf
@@ -15,7 +15,7 @@ module "base_onefinalsound-com" {
 module "zohomail_onefinalsound-com" {
   source = "./modules/zohomail"
 
-  domain_id = linode_domain.onefinalsound-com.id
-  #dkim_public_key   = ""
+  domain_id         = linode_domain.onefinalsound-com.id
+  dkim_public_key   = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCV5bIMucB4IRpLycDWILE9lnerCCNVZMcieg+m0a6e4p99BgFZ9Tk8amkxDkkeYkeNmHgHb4x804dObtfy62jstRS6fUvpOjJ0fMxUmE4iQISQByEOLdPGErs/tE4lQacVlF49I6Z4YyezmxgMploUzpMqVmVz1AFaorDoUWWtzQIDAQAB"
   verification_code = "zb02514618.zmverify.zoho.com"
 }
