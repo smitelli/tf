8e30d721df9c29f92fbd38e70dbf27d819a26418
Scott Smitelli <scott@smitelli.com>
2023-08-12 18:46:16 -0400

    Switch LE and NFM to Zoho mail (from Google)

--- dns-laurenedman-com.tf
+++ dns-laurenedman-com.tf
@@ -14,64 +14,10 @@ module "base_laurenedman-com" {
   primary_ipv6 = [local.alala_ips.v6]
 }
 
-resource "linode_domain_record" "mx_1_laurenedman-com" {
-  domain_id   = linode_domain.laurenedman-com.id
-  record_type = "MX"
-  name        = ""
-  priority    = 1
-  target      = "aspmx.l.google.com"
-}
+module "zohomail_laurenedman-com" {
+  source = "./modules/zohomail"
 
-resource "linode_domain_record" "mx_2_laurenedman-com" {
-  domain_id   = linode_domain.laurenedman-com.id
-  record_type = "MX"
-  name        = ""
-  priority    = 5
-  target      = "alt1.aspmx.l.google.com"
-}
-
-resource "linode_domain_record" "mx_3_laurenedman-com" {
-  domain_id   = linode_domain.laurenedman-com.id
-  record_type = "MX"
-  name        = ""
-  priority    = 5
-  target      = "alt2.aspmx.l.google.com"
-}
-
-resource "linode_domain_record" "mx_4_laurenedman-com" {
-  domain_id   = linode_domain.laurenedman-com.id
-  record_type = "MX"
-  name        = ""
-  priority    = 10
-  target      = "alt3.aspmx.l.google.com"
-}
-
-resource "linode_domain_record" "mx_5_laurenedman-com" {
-  domain_id   = linode_domain.laurenedman-com.id
-  record_type = "MX"
-  name        = ""
-  priority    = 10
-  target      = "alt4.aspmx.l.google.com"
-}
-
-resource "linode_domain_record" "mx_6_laurenedman-com" {
-  domain_id   = linode_domain.laurenedman-com.id
-  record_type = "MX"
-  name        = ""
-  priority    = 15
-  target      = "unmj5boqmpukqikgqwyts5vaqfgfotrvwesedeuv6rymllnevohq.mx-verification.google.com"
-}
-
-resource "linode_domain_record" "cname_webmail-laurenedman-com" {
-  domain_id   = linode_domain.laurenedman-com.id
-  record_type = "CNAME"
-  name        = "webmail"
-  target      = "ghs.googlehosted.com"
-}
-
-resource "linode_domain_record" "txt_spf_laurenedman-com" {
-  domain_id   = linode_domain.laurenedman-com.id
-  record_type = "TXT"
-  name        = ""
-  target      = "v=spf1 include:_spf.google.com ~all"
+  domain_id         = linode_domain.laurenedman-com.id
+  # dkim_public_key   = ""
+  verification_code = "zb36381187.zmverify.zoho.com"
 }
