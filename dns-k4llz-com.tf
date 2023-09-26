826913cceacbbea3adf078c3e3053bf0a5b7d8b1
Scott Smitelli <scott@smitelli.com>
2023-09-26 18:47:59 -0400

    Add k4llz DKIM pubkey

--- dns-k4llz-com.tf
+++ dns-k4llz-com.tf
@@ -16,6 +16,6 @@ module "zohomail_k4llz-com" {
   source = "./modules/zohomail"
 
   domain_id         = linode_domain.k4llz-com.id
-  # dkim_public_key   = ""
+  dkim_public_key   = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCAlv2PB2GISdb5eiF3BlWbZOjFRRqJbwR74IRVDtBeDZrz6TqTnS0nqdyMTJ0xIoZHhOhixwxzp54+EEx65ksUyISmHICnR+lppRfYmEcHx5F7wd08FbyIOZGlr7USr76eQFawaYjP/btj5QtPRlQqeoX00RpGNoKbqlOaogXqUQIDAQAB"
   verification_code = "zb35546922.zmverify.zoho.com"
 }
